e5f979ebfa256e17600510ae62d6f7238e1b0134
zixuan97 <ngzixuan1997@hotmail.com>
2023-11-30 13:34:16 +0800

    initial branding for socialaquarist

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome To Social Aquarist!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          We Love Fish!
         </a>
       </header>
     </div>
